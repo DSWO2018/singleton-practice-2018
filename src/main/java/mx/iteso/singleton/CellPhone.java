b537aad650aa075abf9f72acdd085a55c06e6d54
Javier Torres Lopez <is700845@iteso.mx>
2018-11-24 20:34:43 -0600

    Update CellPhone class

--- src/main/java/mx/iteso/singleton/CellPhone.java
+++ src/main/java/mx/iteso/singleton/CellPhone.java
@@ -17,33 +17,37 @@ public class CellPhone {
      * Send a text.
      * @param msg message.
      * @param phone number.
+     * @return response.
      */
-    public void slowText(final String msg, final String phone) {
-        System.out.println(telcelSIM.post(msg, phone));
+    public String slowText(final String msg, final String phone) {
+        return telcelSIM.post(msg, phone);
     }
 
     /**
      * Send a faster text.
      * @param msg message.
      * @param phone number.
+     * @return response.
      */
-    public void fastText(final String msg, final String phone) {
-        System.out.println(attSIM.post(msg, phone));
+    public String fastText(final String msg, final String phone) {
+        return attSIM.post(msg, phone);
     }
 
     /**
      * Pull data.
      * @param data direction.
+     * @return response.
      */
-    public void pullSlow(final String data) {
-        System.out.println(telcelSIM.get(data));
+    public String pullSlow(final String data) {
+        return telcelSIM.get(data);
     }
 
     /**
      * Pull data faster.
      * @param data direction.
+     * @return response.
      */
-    public void pullFast(final String data) {
-        System.out.println(telcelSIM.get(data));
+    public String pullFast(final String data) {
+        return telcelSIM.get(data);
     }
 }
