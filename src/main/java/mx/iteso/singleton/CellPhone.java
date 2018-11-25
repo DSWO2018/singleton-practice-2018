19214fb5b9b8a2cf51fb40d7e14ff2dd7e70e304
Javier Torres Lopez <is700845@iteso.mx>
2018-11-24 19:10:01 -0600

    Add CellPhone implementation

--- src/main/java/mx/iteso/singleton/CellPhone.java
+++ src/main/java/mx/iteso/singleton/CellPhone.java
@@ -1,4 +1,49 @@
 package mx.iteso.singleton;
 
+/**
+ * CellPhone class.
+ */
 public class CellPhone {
+    /**
+     * ATT SIM connection.
+     */
+    private DataConnection attSIM = ATTConnection.getInstance();
+    /**
+     * Telcel SIM connection.
+     */
+    private DataConnection telcelSIM = TelcelConnection.getInstance();
+
+    /**
+     * Send a text.
+     * @param msg message.
+     * @param phone number.
+     */
+    public void slowText(final String msg, final String phone) {
+        System.out.println(telcelSIM.post(msg, phone));
+    }
+
+    /**
+     * Send a faster text.
+     * @param msg message.
+     * @param phone number.
+     */
+    public void fastText(final String msg, final String phone) {
+        System.out.println(attSIM.post(msg, phone));
+    }
+
+    /**
+     * Pull data.
+     * @param data direction.
+     */
+    public void pullSlow(final String data) {
+        System.out.println(telcelSIM.get(data));
+    }
+
+    /**
+     * Pull data faster.
+     * @param data direction.
+     */
+    public void pullFast(final String data) {
+        System.out.println(telcelSIM.get(data));
+    }
 }
